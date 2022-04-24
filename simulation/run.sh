07b37f4c332c4d34d3b417ac786f901b6ada359e
anudeepsekhar <anudeep.sekhar@gmail.com>
2022-04-23 21:36:36 -0400

    Docker file modified to use tensorflow docker

--- simulation/run.sh
+++ simulation/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-
-docker build -t flower_federated_learning_simulation . && \
-docker run -it --rm flower_federated_learning_simulation
+docker build -t fedl:latest . && \
+docker run --name fedlsim -it -p 8080:8080 --rm -v $PWD:/workspace -w /workspace fedl:latest python simulation.py
