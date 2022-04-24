4901ee4ee0dc23b3c7e6b9d463c773f30feb4c48
Tushar <tvatsa@andrew.cmu.edu>
2022-04-23 22:08:48 -0400

    Adding the rest of the files

---
+++ server.py
@@ -0,0 +1,98 @@
+import flwr as fl
+import torch
+from torch.utils.data import DataLoader, Dataset
+from utils import get_dataset
+from models import CNNMnist, CNNCifar
+import numpy as np
+from client import FlowerClient
+import argparse
+import subprocess, sys
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+class DatasetSplit(Dataset):
+    """An abstract Dataset class wrapped around Pytorch Dataset class.
+    """
+
+    def __init__(self, dataset, idxs):
+        self.dataset = dataset
+        self.idxs = [int(i) for i in idxs]
+
+    def __len__(self):
+        return len(self.idxs)
+
+    def __getitem__(self, item):
+        image, label = self.dataset[self.idxs[item]]
+        return torch.as_tensor(image), torch.as_tensor(label)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type = int, default = 10,
+                        help = "number of rounds of training")
+    parser.add_argument('--local_bs', type = int, default = 64,\
+                        help = "batch size")
+    parser.add_argument('--dataset', type = str, default = 'cifar',\
+                        help = "The dataset to train and evaluate")
+    parser.add_argument('--frac_fit', type = float, default = 1.0,\
+                        help = 'The fraction of clients to choose for training')
+    parser.add_argument('--frac_eval', type = float, default = 0.5,\
+                        help = 'The fraction of clients for evaluation')
+    parser.add_argument('--min_fit_clients', type = int, default = 10,\
+                        help = 'The minimum number of clients for training')
+    parser.add_argument('--min_eval_clients', type = int, default = 10,\
+                        help = 'The minimum number of clients for evaluation')
+    parser.add_argument('--num_users', type = int, default = 100,\
+                        help = 'The number of users')
+    parser.add_argument('--min_available_clients', type = int, default = 10,\
+                        help = 'Wait for these many clients to be available')
+    parser.add_argument('--iid', type = int, default = 0, \
+                        help = '0 for iid and 1 for non iid')
+    args = parser.parse_args()
+
+    #Get the train_dataset, test_dataset, user_group ids for training and validation
+    train_dataset, test_dataset, user_groups_train, user_groups_val = get_dataset(args)
+    trainloaders = []
+    valloaders = []
+
+    #A loop to put all the trainloaders in a list and valloaders in another list
+    for user in range(args.num_users):
+        trainloaders.append(DataLoader(DatasetSplit(train_dataset, user_groups_train[user]),\
+            batch_size = args.local_bs, shuffle = True, num_workers = 4))
+
+        valloaders.append(DataLoader(DatasetSplit(test_dataset, user_groups_val[user]),\
+            batch_size = args.local_bs, shuffle = False, num_workers = 4))
+
+    #Setting the dataset
+    if args.dataset == 'mnist':
+        global_model = CNNMnist(args).to(DEVICE)
+    elif args.dataset == 'cifar':
+        global_model = CNNCifar(args).to(DEVICE)
+
+    #A function to manage the clients.
+    def client_fn(cid):
+        """
+        A custom function to manage all the clients.
+        :param cid: The id for each client
+        :return FlowerClient: Child of the FlowerClient class
+        """
+        net = global_model
+        trainloader = trainloaders[int(cid)]
+        valloader = valloaders[int(cid)]
+        return FlowerClient(net, trainloader, valloader)
+
+    #Setting the strategy for the Clients
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit = args.frac_fit,
+        fraction_eval = args.frac_eval,
+        min_fit_clients = args.min_fit_clients,
+        min_eval_clients = args.min_eval_clients,
+        min_available_clients = args.min_available_clients
+    )
+
+    #Start the Simulation
+    fl.simulation.start_simulation(
+        client_fn = client_fn,
+        num_clients = args.num_users,
+        num_rounds = args.epochs,
+        strategy = strategy
+    )
---
+++ utils.py
@@ -0,0 +1,62 @@
+import copy
+import torch
+from torchvision import datasets, transforms
+from sampling import mnist_iid, mnist_noniid,cifar_iid, cifar_noniid
+
+
+
+def get_dataset(args):
+    """ Returns train and test datasets and a user group which is a dict where
+    the keys are the user index and the values are the corresponding data for
+    each of those users.
+    """
+
+    if args.dataset == 'cifar':
+        data_dir = './data'
+        apply_transform = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+        train_dataset = datasets.CIFAR10(data_dir, train=True, download=True,
+                                       transform=apply_transform)
+
+        test_dataset = datasets.CIFAR10(data_dir, train=False, download=True,
+                                      transform=apply_transform)
+
+        # sample training data amongst users
+        if args.iid == 0:
+            # Sample IID user data from Mnist
+            user_groups_train = cifar_iid(train_dataset, args.num_users)
+            user_groups_val = cifar_iid(test_dataset, args.num_users)
+
+        else:
+            user_groups_train = cifar_noniid(train_dataset, args.num_users)
+            user_groups_val =  cifar_noniid(test_dataset, args.num_users)
+
+    elif args.dataset == 'mnist':
+
+        data_dir = './data'
+
+        apply_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))])
+
+        train_dataset = datasets.MNIST(data_dir, train=True, download=True,
+                                       transform=apply_transform)
+
+        test_dataset = datasets.MNIST(data_dir, train=False, download=True,
+                                      transform=apply_transform)
+
+        # sample training data amongst users
+        if args.iid == 0:
+            # Sample IID user data from Mnist
+            user_groups_train = mnist_iid(train_dataset, args.num_users)
+            user_groups_val = mnist_iid(test_dataset, args.num_users)
+        else:
+
+            user_groups_train = mnist_noniid(train_dataset, args.num_users)
+            user_groups_val = mnist_noniid(test_dataset, args.num_users)
+
+
+    return train_dataset, test_dataset, user_groups_train, user_groups_val
+
