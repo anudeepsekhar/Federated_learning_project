9a56edf865d8902969bcf3765eb7a0f72b5e116a
Tushar <tvatsa@andrew.cmu.edu>
2022-04-23 20:57:52 -0400

    Adding client.py for pytorch branch

---
+++ client.py
@@ -0,0 +1,82 @@
+from collections import OrderedDict
+from typing import Dict, List, Tuple
+import flwr as fl
+import numpy as np
+import torch
+import tqdm
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def train(net, trainloader, epochs: int, verbose=False):
+    """Train the network on the training set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters())
+    net.train()
+    for epoch in range(epochs):
+        correct, total, epoch_loss = 0, 0, 0.0
+        for images, labels in trainloader:
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(net(images), labels)
+            loss.backward()
+            optimizer.step()
+            # Metrics
+            epoch_loss += loss
+            total += labels.size(0)
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+        epoch_loss /= len(trainloader.dataset)
+        epoch_acc = correct / total
+        if verbose:
+            print(f"Epoch {epoch+1}: train loss {epoch_loss}, accuracy {epoch_acc}")
+
+def test(net, testloader):
+    """Evaluate the network on the entire test set."""
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, total, loss = 0, 0, 0.0
+    net.eval()
+    with torch.no_grad():
+        for images, labels in testloader:
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    loss /= len(testloader.dataset)
+    accuracy = correct / total
+    return loss, accuracy
+
+def get_parameters(net) -> List[np.ndarray]:
+    return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+def set_parameters(net, parameters: List[np.ndarray]):
+    params_dict = zip(net.state_dict().keys(), parameters)
+    state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+    net.load_state_dict(state_dict, strict=True)
+
+class FlowerClient(fl.client.NumPyClient):
+    def __init__(self, model, trainloader, valloader):
+        self.model = model
+        self.trainloader = trainloader
+        self.valloader = valloader
+
+
+    def get_parameters(self):
+        return get_parameters(self.model)
+
+
+    def fit(self, parameters, config):
+        set_parameters(self.model, parameters)
+        train(self.model, self.trainloader, epochs = 5)
+        return get_parameters(self.model), len(self.trainloader), {}
+
+    def evaluate(self, parameters, config):
+
+        set_parameters(self.model, parameters)
+        try:
+            loss, accuracy = test(self.model, self.valloader)
+        except Exception as e:
+            print("FAILURE", e)
+
+        return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
