475e3f99a912c79762f01751a59fe1c37b4a0d60
Anudeep Sekhar <anudeep.sekhar@gmail.com>
2022-04-24 21:45:04 -0400

    Update run_client.sh

--- simulation/run_client.sh
+++ simulation/run_client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-docker build -t fedl:latest . && \
-docker run --name fedlsim_$CLIENT_ID -it  --network host --rm -v $PWD:/workspace -w /workspace fedl:latest python client.py --server_address $SERVER_ADDRESS --num_clients 10 --client_id $CLIENT_ID 
+sudo docker build -t fedl:latest . && \
+sudo docker run --name fedlsim_$CLIENT_ID -it  --network host --rm -v $PWD:/workspace -w /workspace fedl:latest python client.py --server_address $SERVER_ADDRESS --num_clients 10 --client_id $CLIENT_ID 
