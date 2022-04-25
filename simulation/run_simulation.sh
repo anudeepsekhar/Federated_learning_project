076c381cd1d1c2f8d629e4714fd44baa6507373a
Anudeep Sekhar <anudeep.sekhar@gmail.com>
2022-04-24 21:46:20 -0400

    Update run_simulation.sh

--- simulation/run_simulation.sh
+++ simulation/run_simulation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-docker build -t fedl:latest . && \
-docker run --name fedlsim -it -p 8080:8080 --rm -v $PWD:/workspace -w /workspace fedl:latest python simulation.py
+sudo docker build -t fedl:latest . && \
+sudo docker run --name fedlsim -it --network host --rm -v $PWD:/workspace -w /workspace fedl:latest python simulation.py
